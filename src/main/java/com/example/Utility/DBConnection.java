35c7ea53adbc72a11c874eb59bb9efcef0cd485b
Ananth <92591290+Ananthbarath@users.noreply.github.com>
2025-01-17 18:59:47 +0530

    Update DBConnection.java

--- src/main/java/com/example/Utility/DBConnection.java
+++ src/main/java/com/example/Utility/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/user_access_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "Berlin@21";
+    private static final String PASSWORD = "Ananth200!";
 
     static {
         try {
